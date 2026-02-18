219cd6af1c268beaa89cbe098459d685590d69b2
Panuganti Sai Siddharth <saisiddharthpanuganti@gmail.com>
2026-02-18 18:56:53 +0530

    Added cors

--- app/main.py
+++ app/main.py
@@ -16,6 +16,17 @@ logger = logging.getLogger("steprealm.main")
 
 app = FastAPI()
 
+from fastapi.middleware.cors import CORSMiddleware
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+
 app.include_router(auth_router, prefix="/auth", tags=["auth"])
 app.include_router(college_router, prefix="/college", tags=["college"])
 app.include_router(mana_router, prefix="/mana", tags=["mana"])
